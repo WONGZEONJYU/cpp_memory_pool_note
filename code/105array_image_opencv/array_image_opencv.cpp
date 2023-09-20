1d4d514bafc9d140518617470101fa1b07aa3af6
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2023-09-20 18:00:12 +0800

    add

--- code/105array_image_opencv/array_image_opencv.cpp
+++ code/105array_image_opencv/array_image_opencv.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+ 
 using namespace std;
 using namespace cv;
 
@@ -9,9 +9,9 @@ int main(int argc, char* argv[])
 	/*
 		400 x 400 的灰度图 做反色 黑色 ==> 白色 
 		一个字节unsigned char 表示灰度 
-		255-灰度 做反色
+		255 - 灰度 做反色
 	*/
-
+	
 	/*补0 用于4字节对齐
 		1 2 3 0
 		4 5 6 0
@@ -19,6 +19,22 @@ int main(int argc, char* argv[])
 		1 2 3 0 4 5 6 0 7 8 9 0
 	*/
 
+	auto img {imread("lena_hed.jpg",IMREAD_GRAYSCALE)};
+	const auto height{ img.rows }, width{ img.cols };
+	cout << "img.elemSize() = " << img.elemSize() << '\n';
+	imshow("test1", img);
+	/*通过指针访问连续的二维数组*/
+	for (int i{}; i < height; ++i) {
+
+		for (int j{}; j < width; ++j) {
+
+			const auto c{ img.data[i * width + j] };
+			img.data[i * width + j] = 255 - c;
+		}
+	}
+
+	imshow("test2",img);
+	waitKey(0);
 	(void)getchar();
 	return 0;
 }

b062e45a967be2ad06a6a42e746a4917258d2001
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2023-09-27 17:21:23 +0800

    add

--- code/109function_arg_ptr/109function_arg_ptr.cpp
+++ code/109function_arg_ptr/109function_arg_ptr.cpp
@@ -3,70 +3,126 @@
 
 using namespace std;
 
-void PtrFunction(const char* data,int size)
+namespace _test_1_
 {
-	cout << "sizeof(data) = " << sizeof(data) << '\n';
+	void PtrFunction(const char* data, int size)
+	{
+		cout << "sizeof(data) = " << sizeof(data) << '\n';
+	}
+
+	/*经过参数传递 数组转换成指针*/
+	void PtrFunction(const char data[])
+	{
+		cout << "sizeof(data) = " << sizeof(data) << '\n';
+	}
+
+	/*传递参数 获取数组大小*/
+	template<typename Ty, size_t SIZE>
+	void TestMen(Ty(&data)[SIZE])
+	{
+		cout << "sizeof(data) = " << sizeof(data) << '\n';
+	}
+
+	/*指针输出*/
+	/*
+		1 修改原内存
+		2 使用函数内创建的内存
+	*/
+
+	char* TestMen()
+	{
+		/*不能返回栈中空间*/
+		char buf[1024]{ "test" };
+		return buf;
+	}
+
+	void TestMen(char* out)
+	{
+		/*修改的是局部变量,内存泄露*/
+		out = new char[1024];
+	}
+
+	/*返回的空间需要注释,由用户释放*/
+	int TestMen(char** out)
+	{
+		int size{ 1024 };
+		*out = new char[size];
+		return size;
+	}
+
+	void test1()
+	{
+		char data[]{ "test mem ptr" };
+
+		PtrFunction(data, sizeof(data));
+		PtrFunction(data);
+		TestMen(data);
+
+		/*异常空间已经释放 , 结果不确定*/
+		cout << TestMen() << '\n';
+
+		char* out{};
+		TestMen(out);
+
+		cout << "out = " << reinterpret_cast<long long>(out) << '\n';
+		TestMen(&out);
+		cout << "out = " << reinterpret_cast<long long>(out) << '\n';
+		delete[] out;
+		out = nullptr;
+		cout << "\n\n";
+	}
 }
 
-/*经过参数传递 数组转换成指针*/
-void PtrFunction(const char data[])
+namespace _test_2_
 {
-	cout << "sizeof(data) = " << sizeof(data) << '\n';
-}
+	/*智能指针作为函数参数和返回值*/
 
-/*传递参数 获取数组大小*/
-template<typename Ty,size_t SIZE>
-void TestMen(Ty (&data)[SIZE])
-{
-	cout << "sizeof(data) = " << sizeof(data) << '\n';
-}
+	class XData
+	{
+		//XData(const XData&) = delete;
+		//XData& operator=(const XData&) = delete;
+	public:
+		XData() { cout << __FUNCTION__ << '\n'; }
+		~XData() { cout << __FUNCTION__ << '\n'; }
+		int index{};
+	};
 
-/*指针输出*/
-/*
-	1 修改原内存
-	2 使用函数内创建的内存
-*/
+	unique_ptr<XData> TestUnique(unique_ptr<XData> ptr)
+	{
+		cout << "prt->index = " << ptr->index << '\n';
+		unique_ptr<XData> re(new XData);
+		re->index = 222;
+		/*编译器优化 1 如果返回值支持move 编译器自动调用move,不支持调用拷贝构造*/
+		return re;
+	}
 
-char* TestMen()
-{
-	/*不能返回栈中空间*/
-	char buf[1024]{ "test" };
-	return buf;
-}
+	void test2()
+	{
+		unique_ptr<XData> ptr1(new XData);
+		ptr1->index = 111;
 
-void TestMen(char* out)
-{
-	/*修改的是局部变量,内存泄露*/
-	out = new char[1024];
-}
+		cout << "ptr1->index = " << ptr1->index << '\n';
+		auto re{ TestUnique(move(ptr1)) };
 
-/*返回的空间需要注释,由用户释放*/
-int TestMen(char** out)
-{
-	int size{ 1024 };
-	*out = new char[size];
-	return size;
+		cout << "re->index = " << re->index << '\n';
+
+		if (!ptr1) {
+			cout << "ptr1 empty\n";
+		}
+		else {
+			cout << "ptr1->index = " << ptr1->index << '\n';
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	char data[]{"test mem ptr"};
+	//_test_1_::test1();
 
-	PtrFunction(data, sizeof(data));
-	PtrFunction(data);
-	TestMen(data);
-	
-	/*异常空间已经释放 , 结果不确定*/
-	cout << TestMen() << '\n';
+	_test_2_::test2();
 
-	char* out{};
-	TestMen(out);
-
-	cout << "out = " << reinterpret_cast<long long>(out) << '\n';
-	TestMen(&out);
-	cout << "out = " << reinterpret_cast<long long>(out) << '\n';
-	delete[] out;
-	out = nullptr;
 	(void)getchar();
 	return 0;
 }
+
+
---
+++ note/6-智能指针作为函数的参数和返回值unique_ptr.md
@@ -0,0 +1,52 @@
+# 6-智能指针作为函数的参数和返回值 `unique_ptr`
+
+# 1.实验代码
+
+[[参考代码]](https://github.com/WONGZEONJYU/cpp_memory_pool_note/tree/main/code/109function_arg_ptr)
+
+>```c++
+>namespace _test_2_
+>{
+>	/*智能指针作为函数参数和返回值*/
+>
+>	class XData
+>	{
+>		//XData(const XData&) = delete;
+>		//XData& operator=(const XData&) = delete;
+>	public:
+>		XData() { cout << __FUNCTION__ << '\n'; }
+>		~XData() { cout << __FUNCTION__ << '\n'; }
+>		int index{};
+>	};
+>
+>	unique_ptr<XData> TestUnique(unique_ptr<XData> ptr)
+>	{
+>		cout << "prt->index = " << ptr->index << '\n';
+>		unique_ptr<XData> re(new XData);
+>		re->index = 222;
+>		/*编译器优化 1 如果返回值支持move 编译器自动调用move,不支持调用拷贝构造*/
+>		return re;
+>	}
+>
+>	void test2()
+>	{
+>		unique_ptr<XData> ptr1(new XData);
+>		ptr1->index = 111;
+>
+>		cout << "ptr1->index = " << ptr1->index << '\n';
+>		auto re{ TestUnique(move(ptr1)) };
+>
+>		cout << "re->index = " << re->index << '\n';
+>
+>		if (!ptr1) {
+>			cout << "ptr1 empty\n";
+>		}
+>		else {
+>			cout << "ptr1->index = " << ptr1->index << '\n';
+>		}
+>	}
+>}
+>```
+>
+><img src="./assets/image-20230927171948248.png" alt="image-20230927171948248" />
+

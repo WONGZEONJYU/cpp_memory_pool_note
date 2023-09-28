04da1be5d7f27308f01aa1a624a93c04a3aeb002
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2023-09-28 11:45:57 +0800

    Update 111_string_vector_mem.cpp

--- code/111_string_vector_mem/111_string_vector_mem.cpp
+++ code/111_string_vector_mem/111_string_vector_mem.cpp
@@ -3,12 +3,31 @@
 
 using namespace std;
 
+class XData
+{
+public:
+	XData() { cout << __FUNCTION__ << '\n'; }
+	~XData() { cout << __FUNCTION__ << '\n'; }
+	
+	XData(const XData& o) {
+		index = o.index;
+		cout << "Copy XData " << index << '\n';
+	}
+
+	XData& operator=(const XData& o) {
+
+		if (this != &o){
+			index = o.index;
+			cout << "=XData " << index << '\n';
+		}
+		return *this;
+	}
+
+	int index{};
+};
+
 int main(int argc, char* argv[])
 {
-
-
-
-
 	(void)getchar();
 	return 0;
 }

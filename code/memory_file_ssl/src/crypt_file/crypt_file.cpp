28bd7c8d2da53c16a60548db4f7894eae0e60135
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2023-10-27 21:08:46 +0800

    add

--- code/memory_file_ssl/src/crypt_file/crypt_file.cpp
+++ code/memory_file_ssl/src/crypt_file/crypt_file.cpp
@@ -8,15 +8,45 @@ int main(int argc, char* argv[])
 	XCrypt crypt;
 	crypt.Init("12345678");
 
-	char out[1024]{};
+	{
+		cout << "====================1 begin======================\n";
 
-	auto en_size{ crypt.Encrypt("abcdefgh",24, out,true) };/*8的倍数*/
-	cout << "en_size = " << en_size << "\nAfter encryption : " << 
-			out << "\n";
+		char out[1024]{};
+		auto en_size{ crypt.Encrypt("abcdefghijklmn",24, out,true) };/*8的倍数*/
+		cout << "en_size = " << en_size << 
+			"\nAfter encryption : " << out << "\n";
 
-	en_size = crypt.Encrypt("abcdefg", 9, out, true);/*非8的倍数*/
-	cout << "en_size = " << en_size << "\nAfter encryption : " <<
+		char de_out[1024]{};
+		auto de_size{ crypt.Decrypt(out, en_size,de_out, true) };
+		cout << "de_size = " << de_size <<
+				"\nde_out = " << de_out << "\n";
+
+		cout << "====================1 end======================\n";
+	}
+
+	{
+		char out[1024]{};
+		auto en_size{ crypt.Encrypt("abcdefgh",8, out,true) };
+		cout << "en_size = " << en_size << "\After encryption : " <<
 		out << "\n";
+
+		char de_out[1024]{};
+		auto de_size{ crypt.Decrypt(out, en_size,de_out, true) };
+		cout << "de_size = " << de_size <<
+			"\nde_out = " << de_out << "\n";
+	}
+
+	{
+		char out[1024]{};
+		auto en_size = crypt.Encrypt("abcdefghi", 9, out, true);
+		cout << "en_size = " << en_size << 
+				"\nAfter encryption : " << out << "\n";
+
+		char de_out[1024]{};
+		auto de_size{ crypt.Decrypt(out, en_size,de_out, true) };
+		cout << "de_size = " << de_size <<
+			"\nde_out = " << de_out << "\n";
+	}
 
 	(void)getchar();
 	return 0;
--- code/memory_file_ssl/src/crypt_file/xcrypt.cpp
+++ code/memory_file_ssl/src/crypt_file/xcrypt.cpp
@@ -1,7 +1,10 @@
 #include "xcrypt.h"
+#include <iostream>
+
+using namespace std;
 
 /*初始化密钥 , DES加密算法 密钥最多8bit 多余丢弃不足补0*/
-bool XCrypt::Init(std::string password)
+bool XCrypt::Init(string password)
 {
     const_DES_cblock key{}; /*不足补0,初始化就置0*/
     auto key_size{ password.size()};
@@ -25,7 +28,7 @@ bool XCrypt::Init(std::string password)
 /// <returns>0 or size 返回加密后数据大小, 有可能大于输入 , 添加补充</returns>
 /// 
 /// 
-size_t XCrypt::Encrypt(const char* in_data, const size_t insize, char* out_data, bool is_end)
+size_t XCrypt::Encrypt(const char* in_data, const size_t insize, char* out_data, const bool is_end)
 {
     if ((!in_data) || (!out_data) || (insize <= 0)) {
         return 0;
@@ -38,19 +41,20 @@ size_t XCrypt::Encrypt(const char* in_data, const size_t insize, char* out_data,
 
     for (size_t i{}; i < insize; i+= block_size){
 
-        const_DES_cblock in{}; /*输入(未加密数据)*/
-        DES_cblock out{};/*输出(加密后数据)*/
-
         const auto temp_v{ insize - i };
 
         data_size = (temp_v < block_size) ? temp_v : block_size;
 
+        const_DES_cblock in{}; /*输入(未加密数据)*/
+        DES_cblock out{};/*输出(加密后数据)*/
+
         memcpy(in, in_data + write_size, data_size);
 
         /*填充 补充的数据大小 @@@@@@@1 @@@55555 @@@@@@@@88888888*/
-        const auto r{ is_end && ((i + block_size) >= insize) };
 
-        if (r){
+        const auto is_{ is_end && ((i + block_size) >= insize) };
+
+        if (is_){
             //@@@@@@@@8888888
             if (padding == block_size) { /*补充多8个字节*/
                 DES_ecb_encrypt(&in, &out, &key_sch_, DES_ENCRYPT);
@@ -70,7 +74,62 @@ size_t XCrypt::Encrypt(const char* in_data, const size_t insize, char* out_data,
     return write_size;
 }
 
-size_t XCrypt::Decrypt(const char* in_data, size_t insize, char* out_data, bool is_end)
+/// <summary>
+/// 解密数据,结尾去掉填充大小
+/// </summary>
+/// <param name="in_data">输入数据</param>
+/// <param name="insize">输入数据大小</param>
+/// <param name="out_data">输出数据</param>
+/// <param name="is_end">是否到加密结尾</param>
+/// <returns>-1 or size 返回解密后数据大小 , 有可能小于输入 , 去掉补充</returns>
+
+size_t XCrypt::Decrypt(const char* in_data, const size_t insize, char* out_data, const bool is_end)
 {
-    return 0;
+    if ((!in_data) || (!out_data) || (insize <= 0)) {
+        return 0;
+    }
+
+    const auto block_size{ sizeof(const_DES_cblock) }, over{ insize % block_size },
+        padding{ block_size - over };
+
+    size_t write_size{}, data_size{};
+
+    for (size_t i {}; i < insize; i+= block_size) {
+
+        const_DES_cblock in{}; /*输入(已加密数据)*/
+        DES_cblock out{};/*输出(解密后数据)*/
+
+        memcpy(&in, in_data + write_size, block_size);
+        DES_ecb_encrypt(&in, &out, &key_sch_, DES_DECRYPT); /*解密*/
+
+        data_size = block_size;/*默认情况data_size == block_size*/
+
+        /*处理结尾处*/
+
+        /*如果数据长度不是8的倍数，那么最后一块数据不足8个字节的位置都会被填充为剩余的长度大小这个数值
+        * 比如 @@@55555 , 5是从第三个位置开始填充,到结尾也是5,所以out[7] == 5
+        */
+
+        const auto is_ { is_end && ((insize - i) <= block_size)};
+
+        if (is_){
+
+            data_size = block_size - out[7]; 
+            /*通过最后一个元素的值可以计算出还需拷贝多少个数据*/
+
+            if (!data_size){ /*如果是8的倍数,data_size == 0*/
+                break;
+            }else if(data_size < 0){ /*异常情况*/
+                cerr << "Decrypt failed!padding size error\n";
+                break;
+            }else { /*正常情况*/
+
+            }
+        }
+
+        memcpy(out_data + write_size, &out, data_size);
+        write_size += block_size;
+    }
+
+    return write_size;
 }
